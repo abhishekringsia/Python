52bc4a0b2ba2d8860cf22776ae396b08da7ea17d
Abhishek Ringsia <abhishek.ringsia@autodesk.com>
2019-01-27 17:16:27 +0530

    ml

---
+++ Basic Python/com/abhishek/edureka1.py
@@ -0,0 +1,7 @@
+a=A=10
+A=20
+print(a)
+print(A)
+
+mod_5 = lambda x : x % 5
+print("mode is ", mod_5(101))
---
+++ Basic Python/com/abhishek/kaggleLibraries.py
@@ -0,0 +1,14 @@
+import math;
+import numpy;
+
+x = "a{}bc{}"
+print(x.format(",","."))
+
+print("this is how format works {}".format(" a "));
+print("pi to 4 digits  = {:.4}".format(math.pi))
+
+list =[1,2,2,3]
+print(type(list) , list)
+
+array = numpy.array(list)
+print(type(array) , array)
